97c26b03c6cccaeb165e29408488d97460ace25b
Pedro Hurtado <pedrohurt@gmail.com>
2025-09-08 12:43:23 +0200

    error responses in update customers

--- customers/src/customers/features/customer/commands/update.py
+++ customers/src/customers/features/customer/commands/update.py
@@ -2,6 +2,7 @@ from common.server import build_router,empty
 from common.mediator import Command
 from common.ioc import inject, deps
 from common.confg import Config
+from common.openapi import build_error_responses
 from common.server import empty
 
 
@@ -10,7 +11,7 @@ router = build_router("customers")
 class Request(Command):...    
 
 
-@router.put("", summary="Update Customer", status_code=204)
+@router.put("", summary="Update Customer", status_code=204, responses=build_error_responses(400,409))
 @inject
 def controller(req:Request, config:Config=deps(Config)):
     return empty()
